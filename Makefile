5eb39a8674a9714568b237c0c44a16904d6499e1
Aurutus <emslhy@hotmail.com>
2023-01-08 11:29:24 +0800

    Add new init scripts for creating test_snippets

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
 
 .PHONY: init
 init:
+# if the project is new cloned from remote repo, the tstr_entry.go will be missing
 ifeq (,$(wildcard ./tstr_entry.go))
 	./scripts/init_dojo.sh
 endif
+
+.PHONY: init_tstr
+# init the new tester and its go file under ./src/test_snippets
+init_tstr:
+	./scripts/init_test_snippet.sh $(file) $(tstr)
 
 .PHONY: build
 build: init
--- scripts/init_test_snippet.sh
+++ scripts/init_test_snippet.sh
@@ -3,6 +3,7 @@
 TSTR_FILE=$1
 TSTR=$2
 
+# Create new tester file and tstr struct
 cat > ./src/test_snippets/${TSTR_FILE} << EOF
 package test_snippets
 
@@ -16,8 +17,24 @@ type $TSTR struct{}
 var _ snippets.Tstr = (*$TSTR)(nil)
 
 func (tstr *$TSTR) Test(ctx context.Context) error {
+	log := cfmt.Logger(ctx)
 	return nil
 }
 
+EOF
+
+# Change the test entry
+cat > ./tstr_entry.go << EOF
+package main
+
+import (
+	snippets "snippets_dojo/src"
+	test_snippets "snippets_dojo/src/test_snippets"
+)
+
+func NewTesterEntry() snippets.Tstr {
+	// !NOTE: customize tester entrance here
+	return &test_snippets.$TSTR{}
+}
 
 EOF
